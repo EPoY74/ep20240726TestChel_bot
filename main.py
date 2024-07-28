c86cfc40d439dc3a6395b3fe2950f5119feeaea6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-07-29 01:38:28 +0500

    Добавил комментарии. Дописал функцию, которая открывает наш сайт.

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 
 """
 
+from webbrowser import open as web_open
+
 from telebot import TeleBot
+
+
 
 MY_TELEGRAM_API='7341698907:AAGlo8L4epgwUtOyXo31x-6wF4eVMRBmlj8'
 bot = TeleBot(MY_TELEGRAM_API)
@@ -43,7 +47,21 @@ def message_sys_info(message):
     bot.send_message(message.chat.id, message)
 
 
-# Обрабатываю текст введенный пользователем
+@bot.message_handler(commands=["site", "website"])
+def send_site(massage):
+    """
+    Send to user our website
+    :param massage:
+    :return:
+    """
+    web_open("mail.ru")
+
+
+# Обрабатываю текст введенный пользователе
+# ставить только после обработки всех команд!
+# Если поставить до обработки команд, то все сообщения
+# Даже команды будут обрабатываться не как команды,
+# а как просто текст
 @bot.message_handler()
 def processing_user_text(message):
     """
@@ -55,6 +73,9 @@ def processing_user_text(message):
     """
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, f"Привет {message.from_user.first_name}")
+    elif message.text.lower() == "id":
+        # Так отвечаем нп конкретное сообщение
+        bot.reply_to(message, f"id: {message.from_user.id}")
 
 
 if __name__ == "__main__":
