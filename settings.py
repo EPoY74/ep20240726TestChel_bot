203374b18998a8198c5afd74043c6e88ca85c3da
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-08-04 00:34:50 +0500

    Выделил настройки в отдельный файл

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""
+Файл настроек для телеграмм бота
+Адрес урока (одного из):
+https://www.youtube.com/watch?v=RpiWnPNTeww
+Библиотека: pyTelegramBotAPI
+"""
+from telebot import TeleBot
+
+# Токен от телеграмм бота
+MY_TELEGRAM_API = '7341698907:AAGlo8L4epgwUtOyXo31x-6wF4eVMRBmlj8'
+bot = TeleBot(MY_TELEGRAM_API)
