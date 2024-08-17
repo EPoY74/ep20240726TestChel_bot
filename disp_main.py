fbd34bd9ac2f5b9809aaaee7c081f3fbfcdc597a
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-08-17 23:10:07 +0500

    Поправил замечания от линтера. Добавил функцию чтения данных из БД

--- disp_main.py
+++ disp_main.py
@@ -16,15 +16,16 @@ def getting_time() -> typing.List:
     Возвращает текущее время в неформатированном виде
     :return:
     """
-    time_now_get = time.localtime(time.time())
-    time_now_out_get: typing.List = [time_now_get.tm_mday,
-                              time_now_get.tm_mon,
-                              time_now_get.tm_year,
-                              time_now_get.tm_hour,
-                              time_now_get.tm_min,
-                              time_now_get.tm_sec,
-                              ]
+    ep_current_time = time.localtime(time.time())
+    time_now_out_get: typing.List = [ep_current_time.tm_mday,
+                                     ep_current_time.tm_mon,
+                                     ep_current_time.tm_year,
+                                     ep_current_time.tm_hour,
+                                     ep_current_time.tm_min,
+                                     ep_current_time.tm_sec,
+                                     ]
     return time_now_out_get
+
 
 def write_to_db(sql_con: str):
     """
@@ -54,7 +55,7 @@ def write_to_db(sql_con: str):
 
 def read_all_from_db(sql_con: str):
     """
-    Чтение запроса из  БД PostgresQL
+    Чтение запроса из БД PostgresQL.
     Возвращает все найденные строки из запроса
 
     sql_con: str - передаваемый sql запрос
@@ -73,13 +74,31 @@ def read_all_from_db(sql_con: str):
         print(f"БД подключена {getting_time()}")
         with con.cursor() as curr:
             curr.execute(sql_con)
-            curr.fetchall()
+            read_all_results = curr.fetchall()
             print(f"Запрос {sql_con} выполнен в {getting_time()}")
         con.close()
         print(f"Connection is closed {getting_time()}")
+        return read_all_results
     except psy.Error as err:
         print(f"Ошибка: \n:{err}\n{getting_time()}")
         raise err
 
+
+def get_table_datas(table_name: str):
+    """
+    Читаю название таблицы 
+    """
+    sql_query = f"""
+    SELECT
+    *
+    FROM
+        {table_name};
+    """
+    return read_all_from_db(sql_query)
+
+
 if __name__ == "__main__":
     print(f"Диспетчер запустил программу в {getting_time()}")
+    results = get_table_datas('telegramm_user')
+    for result in results:
+        print(result)
