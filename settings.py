e98fa9c4d3dac72092e4b5dacaed644a85570530
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-09-07 08:16:04 +0500

    добавил более полные комментарии в файл настроек

--- settings.py
+++ settings.py
@@ -6,12 +6,31 @@ https://www.youtube.com/watch?v=RpiWnPNTeww
 """
 import os
 
-# Токен от телеграмм бота
+# Считываю переменную окружения из ОС с
+# токеном подключения к telegramm MY_TELEGRAM_API_202407_TEST_BOT
 MY_TELEGRAM_API = os.getenv("MY_TELEGRAM_API_202407_TEST_BOT")
 
-# Параметры подключения к БД postgresQL
+# Считываю параметры подключеия к БД из
+#  переменных окружения операционой системы.
+# Они расположены вне кода с целью обеспечения безопасности
+#  и снижения риска утечки конфиденциальной информации
+
+# Считываю переменную окружения из ОС с
+# наименованием БД MY_TELEGRAM_BOT_DBNAME_202407_TEST
 MY_TELEGRAM_BOT_DBNAME = os.getenv("MY_TELEGRAM_BOT_DBNAME_202407_TEST")
+
+# Считываю переменную окружения из ОС с
+# именем пользователя БД MY_TELEGRAM_BOT_USER_202407_TEST
 MY_TELEGRAM_BOT_USER = os.getenv("MY_TELEGRAM_BOT_USER_202407_TEST")
+
+# Считываю переменную окружения из ОС с 
+# перолем от БД  MY_TELEGRAM_BOT_PASSWORD_202407_TEST
 MY_TELEGRAM_BOT_PASSWORD = os.getenv("MY_TELEGRAM_BOT_PASSWORD_202407_TEST")
+
+# Считываю переменную окружения из ОС с
+# хостом расположеня БД MY_TELEGRAM_BOT_HOST_202407_TEST
 MY_TELEGRAM_BOT_HOST = os.getenv("MY_TELEGRAM_BOT_HOST_202407_TEST")
+
+# Считываю переменную окружения из ОС с
+# портом подключения к БД MY_TELEGRAM_BOT_PORT_202407_TEST
 MY_TELEGRAM_BOT_PORT = os.getenv("MY_TELEGRAM_BOT_PORT_202407_TEST")
